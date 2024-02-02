6dde8a9e805cbee70832faf3c5d181bd06ecdae3
BassoSulle <bassoelibariki56@gmail.com>
2024-02-02 09:03:50 +0300

    added a new route to fetch student project information from the database. Also updated the viewprojects.pug file to display the fetched data in a table.

--- routes/viewprojects.js
+++ routes/viewprojects.js
@@ -6,5 +6,18 @@ router.get('/',(req, res) => {
     res.render("viewprojects");
 });
 
+app.get('/', (req, res) => {
+    console.log(req.body)
+    
+    const sql = 'SELECT * FROM student_project';
+
+    connection.query(sql, (err, result) => {
+      if (err) 
+      {
+        throw err;
+      }
+      res.json(result);
+    });
+  });
 
 module.exports = router;
